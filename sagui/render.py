4c97c354040c0734d0b97d360c6507f579a52186
mzubia <markel.xubi@gmail.com>
2023-11-16 21:21:30 +0100

    Add render test to sagui

---
+++ sagui/render.py
@@ -0,0 +1,22 @@
+import safety_gymnasium
+
+
+env = safety_gymnasium.make('SafetyPointGoal1-v0', render_mode='human')
+obs, info = env.reset()  # pylint: disable=unused-variable
+# Use below to specify seed.
+# obs, _ = env.reset(seed=0)
+terminated, truncated = False, False
+ep_ret, ep_cost = 0, 0
+while True:
+    if terminated or truncated:
+        print(f'Episode Return: {ep_ret} \t Episode Cost: {ep_cost}')
+        ep_ret, ep_cost = 0, 0
+        obs, info = env.reset()  # pylint: disable=unused-variable
+    assert env.observation_space.contains(obs)
+    act = env.action_space.sample()
+    assert env.action_space.contains(act)
+    # pylint: disable-next=unused-variable
+    obs, reward, cost, terminated, truncated, info = env.step(act)
+
+    ep_ret += reward
+    ep_cost += cost
