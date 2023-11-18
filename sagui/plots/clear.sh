d77b293e98824ca0a297fa0b346ecbcde1816a44
mzubia <markel.xubi@gmail.com>
2023-11-18 17:23:25 +0100

    Add plotting scripts

---
+++ sagui/plots/clear.sh
@@ -0,0 +1,1 @@
+rm *.png progress.csv
---
+++ sagui/plots/plot_progress.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the CSV file into a DataFrame
+df = pd.read_csv("progress.csv", delimiter=",")
+
+
+def plot_and_save(column: str, scale=100):
+    x_label = 'TotalEnvSteps'
+    plt.figure(figsize=(10, 5))
+    plt.plot(df[x_label].values, df[column].values)
+    plt.scatter(df[x_label].values, df[column].values)
+    plt.xlabel(x_label)
+    plt.ylabel(column)
+    plt.title(column)
+    plt.ylim(-5, scale)
+    plt.grid(True)
+
+    fname = column.replace('/', '-')
+    plt.savefig(fname + ".png")
+    # plt.show()
+
+
+# Plot AverageEpRet
+plot_and_save("Metrics/EpRet", scale=75)
+
+# Plot AverageEpCost
+plot_and_save("Metrics/EpCost", scale=50)
+
+# Plot AverageTestEpCost
+plot_and_save("Metrics/TestEpCost", scale=50)
+
+# Plot LossAlpha
+# plot_and_save("PiEntropy")
