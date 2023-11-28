1cdee1d48438c9316ed13bc3747b13cf07126383
mzubia <markel.xubi@gmail.com>
2023-11-28 13:46:31 +0100

    Update mygoal envs

--- omnisafe/envs/sagui_envs.py
+++ omnisafe/envs/sagui_envs.py
@@ -76,7 +76,7 @@ class MygoalLevel1(BaseTask):
 
         self._add_geoms(Goal(keepout=0.305))
         self._add_geoms(Hazards(num=8, keepout=0.22))
-        self._add_geoms(Sigwalls(num=4, locate_factor=3.2, is_constrained=True))
+        self._add_geoms(Sigwalls(num=4, locate_factor=5., is_constrained=True))
         self._add_free_geoms(Vases(num=1, is_constrained=False, keepout=0.18))
 
         self.placements_conf.extents = [-2, -2, 2, 2]
@@ -126,29 +126,30 @@ class MygoalLevel2(BaseTask):
     def __init__(self, config) -> None:
         super().__init__(config=config)
 
-        self._add_geoms(Goal(keepout=0.305))
+        # self._add_geoms(Goal(keepout=0.305))
         self._add_geoms(Hazards(num=8, keepout=0.22))
         self._add_geoms(Sigwalls(num=4, locate_factor=3.2, is_constrained=True))
         self._add_free_geoms(Vases(num=1, is_constrained=False, keepout=0.18))
 
-        self.placements_conf.extents = [-2, -2, 2, 2]
+        dist = 1.75
+        self.placements_conf.extents = [-dist, -dist, dist, dist]
 
-        self.last_dist_goal = None
+        # self.last_dist_goal = None
 
-        self.hazards.num = 4
-        self.vases.num = 4
+        self.hazards.num = 3
+        self.vases.num = 3
         self.vases.is_constrained = True
 
     def calculate_reward(self):
         """Determine reward depending on the agent and tasks."""
         # pylint: disable=no-member
         reward = 0.0
-        dist_goal = self.dist_goal()
-        reward += (self.last_dist_goal - dist_goal) * self.goal.reward_distance
-        self.last_dist_goal = dist_goal
+        # dist_goal = self.dist_goal()
+        # reward += (self.last_dist_goal - dist_goal) * self.goal.reward_distance
+        # self.last_dist_goal = dist_goal
 
-        if self.goal_achieved:
-            reward += self.goal.reward_goal
+        # if self.goal_achieved:
+        #     reward += self.goal.reward_goal
         return reward
 
     def specific_reset(self):
@@ -159,11 +160,11 @@ class MygoalLevel2(BaseTask):
 
     def update_world(self):
         """Build a new goal position, maybe with resampling due to hazards."""
-        self.build_goal_position()
-        self.last_dist_goal = self.dist_goal()
+        # self.build_goal_position()
+        # self.last_dist_goal = self.dist_goal()
 
     @property
     def goal_achieved(self):
         """Whether the goal of task is achieved."""
         # pylint: disable-next=no-member
-        return self.dist_goal() <= self.goal.size
+        return False  # self.dist_goal() <= self.goal.size
