3923dcb3d54fa1a0873b85289e478f0009c244ce
mzubia <markel.xubi@gmail.com>
2023-12-01 20:11:20 +0100

    Add new environment SafetyPointGuide2-v0

--- omnisafe/envs/sagui_envs.py
+++ omnisafe/envs/sagui_envs.py
@@ -9,6 +9,7 @@ from safety_gymnasium.utils.registration import register
 
 sagui_env_ids = ['SafetyPointGuide0-v0',
                  'SafetyPointGuide1-v0',
+                 'SafetyPointGuide2-v0',
                  'SafetyPointMygoal1-v0',
                  'SafetyPointMygoal2-v0',
                  'SafetyPointMygoal3-v0']
@@ -81,6 +82,53 @@ class GuideLevel1(BaseTask):
         self._add_geoms(Sigwalls(num=4, locate_factor=4, is_constrained=True))
 
         self.placements_conf.extents = [-2, -2, 2, 2]
+
+    def calculate_reward(self):
+        """Determine reward depending on the agent and tasks."""
+        # pylint: disable=no-member
+        reward = 0.0
+
+        # Distance bonus here
+
+        return reward
+
+    def specific_reset(self):
+        pass
+
+    def specific_step(self):
+        pass
+
+    def update_world(self):
+        """Build a new goal position, maybe with resampling due to hazards."""
+        # self.build_goal_position()
+        # self.last_robot_pos = self.agent.pos()
+
+    @property
+    def goal_achieved(self):
+        """Whether the goal of task is achieved."""
+        # pylint: disable-next=no-member
+        return False  # self.dist_goal() <= self.goal.size
+
+
+# Took it from
+# https://github.com/PKU-Alignment/safety-gymnasium/tree/main/safety_gymnasium/tasks
+class GuideLevel2(BaseTask):
+    """An agent must navigate to a goal."""
+
+    def __init__(self, config) -> None:
+        super().__init__(config=config)
+
+        # self._add_geoms(Goal(keepout=0.305))
+        self._add_geoms(Hazards(num=8, keepout=0.5))
+        self._add_geoms(Sigwalls(num=4, locate_factor=3.2, is_constrained=True))
+        # self._add_free_geoms(Vases(num=1, is_constrained=False, keepout=0.18))
+
+        # self.last_robot_pos = None
+        self.placements_conf.extents = [-2, -2, 2, 2]
+
+        self.hazards.num = 5
+        # self.vases.num = 8
+        # self.vases.is_constrained = True
 
     def calculate_reward(self):
         """Determine reward depending on the agent and tasks."""
