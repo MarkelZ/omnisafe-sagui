739732d97c07d7be8b93681ca5cf4842084712f7
mzubia <markel.xubi@gmail.com>
2023-12-10 20:27:23 +0100

    Implement DDPG based robustified algorithms

--- omnisafe/algorithms/sagui/__init__.py
+++ omnisafe/algorithms/sagui/__init__.py
@@ -4,6 +4,9 @@ from omnisafe.algorithms.sagui.sac_lagb import SACLagB
 from omnisafe.algorithms.sagui.rand_act import SACLagRandAct
 from omnisafe.algorithms.sagui.rand_obs import SACLagRandObs
 from omnisafe.algorithms.sagui.sagui_cs import SaGuiCS
+from omnisafe.algorithms.sagui.ddpg_lag_unfold import DDPGLagUnfold
+from omnisafe.algorithms.sagui.ddpg_lag_randact import DDPGRandAct
+from omnisafe.algorithms.sagui.ddpg_lag_randobs import DDPGRandObs
 
 
-__all__ = ['SACLagB', 'SACLagRandAct', 'SACLagRandObs', 'SaGuiCS']
+__all__ = ['DDPGLagUnfold', 'DDPGRandAct', 'DDPGRandObs', 'SACLagB', 'SACLagRandAct', 'SACLagRandObs', 'SaGuiCS']
