aabd771d9eb157878215de24f6c20dd9a1fd9b59
MarkelZ <markel.xubi@gmail.com>
2023-12-14 22:12:56 +0100

    Train multiple agents

--- sagui/train_guide.py
+++ sagui/train_guide.py
@@ -3,21 +3,22 @@ from omnisafe.envs.sagui_envs import register_sagui_envs
 
 
 if __name__ == '__main__':
-    register_sagui_envs()
-    env_id = 'SafetyPointGuide0-v0'
+    envs = ['SafetyPointGuide0-v0', 'SafetyPointGuide1-v0', 'SafetyPointGuide2-v0']
+    algos = ['DDPGLagUnfold', 'DDPGRandAct']
+    
+    for alg in algos:
+        for env_id in envs:
+            register_sagui_envs()
 
-    cfgs = {
-        'train_cfgs': {
-            'torch_threads': 8
-        },
-        'logger_cfgs': {
-            'save_model_freq': 25
-        },
-    }
+            cfgs = {
+                'logger_cfgs': {
+                    'save_model_freq': 25
+                },
+            }
 
-    agent = omnisafe.Agent('DDPGLagUnfold', env_id)
-    agent.learn()
+            agent = omnisafe.Agent(alg, env_id)
+            agent.learn()
 
-    agent.plot(smooth=1)
-    agent.render(num_episodes=1, render_mode='rgb_array', width=256, height=256)
-    agent.evaluate(num_episodes=1)
+            agent.plot(smooth=1)
+            agent.render(num_episodes=1, render_mode='rgb_array', width=256, height=256)
+            agent.evaluate(num_episodes=1)
