02ff742b3118b8da6a404582bce4cc927626db9c
mzubia <markel.xubi@gmail.com>
2023-12-16 18:23:05 +0100

    Update hyperparameters

--- sagui/train_guide.py
+++ sagui/train_guide.py
@@ -5,18 +5,18 @@ from omnisafe.envs.sagui_envs import register_sagui_envs
 if __name__ == '__main__':
     envs = ['SafetyPointGuide0-v0', 'SafetyPointGuide1-v0', 'SafetyPointGuide2-v0']
     algos = ['DDPGLagUnfold', 'DDPGRandAct']
-    
+
     for alg in algos:
         for env_id in envs:
             register_sagui_envs()
 
-            cfgs = {
+            custom_cfgs = {
                 'logger_cfgs': {
                     'save_model_freq': 25
                 },
             }
 
-            agent = omnisafe.Agent(alg, env_id)
+            agent = omnisafe.Agent(alg, env_id, custom_cfgs=custom_cfgs)
             agent.learn()
 
             agent.plot(smooth=1)
