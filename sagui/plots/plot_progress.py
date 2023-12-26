ec2722b96830d0352c4e982d2c729ead881865d9
mzubia <markel.xubi@gmail.com>
2023-12-26 12:18:47 +0100

    Plot progress now plots every .csv file in the working directory

--- sagui/plots/plot_progress.py
+++ sagui/plots/plot_progress.py
@@ -1,31 +1,46 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-# Read the CSV file into a DataFrame
-df = pd.read_csv("progress.csv", delimiter=",")
+from pathlib import Path
 
 
-def plot_and_save(column: str, scale=100):
-    x_label = 'TotalEnvSteps'
-    plt.figure(figsize=(10, 5))
-    plt.plot(df[x_label].values, df[column].values)
-    plt.scatter(df[x_label].values, df[column].values)
+def subplot(df, algo, x_label, y_label):
+    x = df[x_label].values
+    y = df[y_label].values
+
+    plt.plot(x, y, label=algo)
+    plt.scatter(x, y, label=algo)
     plt.xlabel(x_label)
-    plt.ylabel(column)
-    plt.title(column)
-    plt.ylim(-5, scale)
+    plt.ylabel(y_label)
+    plt.title(y_label)
     plt.grid(True)
-
-    fname = column.replace('/', '-')
-    plt.savefig(fname + ".png")
-    # plt.show()
+    plt.legend()
 
 
-# Plot progress
-plot_and_save("Metrics/EpRet", scale=75)
-plot_and_save("Metrics/EpCost", scale=50)
-plot_and_save("Metrics/TestEpRet", scale=50)
-plot_and_save("Metrics/TestEpCost", scale=50)
+# Get the current directory as a Path object
+current_directory = Path.cwd()
 
-# Plot LossAlpha
-# plot_and_save("PiEntropy")
+# Specify the pattern for CSV files
+csv_pattern = '*.csv'
+
+# Use the glob method of the Path object to find all files matching the pattern
+csv_files = current_directory.glob(csv_pattern)
+
+# Display the list of CSV files
+for csv_file in csv_files:
+    fname = csv_file.name
+    algo = fname[9:-4]
+    df = pd.read_csv(fname, delimiter=",")
+
+    plt.figure(figsize=(15, 5))
+
+    x_label = 'TotalEnvSteps'
+
+    y_label = 'Metrics/EpRet'
+    plt.subplot(1, 2, 1)  # 1 row, 2 columns, subplot 1
+    subplot(df, algo, x_label, y_label)
+
+    y_label = 'Metrics/EpCost'
+    plt.subplot(1, 2, 2)  # 1 row, 2 columns, subplot 2
+    subplot(df, algo, x_label, y_label)
+
+    plt.savefig(algo + '.png')
