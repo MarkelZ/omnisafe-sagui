2da0d4ea7b9849d20320a202358428e7f892a8d1
mzubia <markel.xubi@gmail.com>
2023-12-30 01:39:34 +0100

    Let get_saves script take optional arguments

--- sagui/get_saves.sh
+++ sagui/get_saves.sh
@@ -1,3 +1,6 @@
-wget https://raw.githubusercontent.com/MarkelZ/robust-models/main/saves.zip
-unzip saves.zip
-rm saves.zip
+default_fname="saves.zip"
+fname="${1:-$default_fname}"
+
+wget "https://raw.githubusercontent.com/MarkelZ/robust-models/main/$fname"
+unzip "$fname"
+rm "$fname"
