0da7cf41dd0275406f8e2995d7524e55100d1fb9
mzubia <markel.xubi@gmail.com>
2024-01-03 20:55:40 +0100

    Add SACLagUnfold aglorithm

--- omnisafe/algorithms/sagui/__init__.py
+++ omnisafe/algorithms/sagui/__init__.py
@@ -9,7 +9,8 @@ from omnisafe.algorithms.sagui.ddpg_lag_randact import DDPGRandAct
 from omnisafe.algorithms.sagui.ddpg_lag_randobs import DDPGRandObs
 from omnisafe.algorithms.sagui.ddpg_lag_probact import DDPGProbAct
 from omnisafe.algorithms.sagui.ddpg_lag_probobs import DDPGProbObs
+from omnisafe.algorithms.sagui.sac_lag_unfold import SACLagUnfold
 
 
 __all__ = ['DDPGLagUnfold', 'DDPGRandAct', 'DDPGRandObs', 'DDPGProbAct', 'DDPGProbObs',
-           'SACLagB', 'SACLagRandAct', 'SACLagRandObs', 'SaGuiCS']
+           'SACLagB', 'SACLagRandAct', 'SACLagRandObs', 'SaGuiCS', 'SACLagUnfold']
