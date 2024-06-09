6d147e6ad41fef8ccdb8d35ba931a4787d73657e
mzubia <markel.xubi@gmail.com>
2024-06-09 15:05:44 +0200

    Update plot_coefs to use the new coefficient intervals

--- sagui/plots/plot_coefs.py
+++ sagui/plots/plot_coefs.py
@@ -1,17 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 import safety_gymnasium
 from safety_gymnasium.tasks.safe_navigation.goal.goal_level0 import GoalLevel0
 from safety_gymnasium.bases.base_task import BaseTask
+
+from omnisafe.envs.sagui_envs import register_sagui_envs
 
 
 # Modify the physics constants of the environment
 def modify_dyn(task: BaseTask, coef_dic: dict):
     model = task.model
-    for coef, val in coef_dic.items():
-        atr = getattr(model, coef)
-        for index, value in np.ndenumerate(atr):
-            atr[index] = value + val
+
+    for name, mult in coef_dic.items():
+        atr: np.ndarray = getattr(model, name)
+        atr[:] *= mult
 
 
 # Nice to have
@@ -20,23 +23,26 @@ def print_attr(obj):
     print('\n'.join(attr))
 
 
-# Create a 4x4 grid of subplots
-size = 4
+# Create a 3x3 grid of subplots
+size = 3
 fig, axs = plt.subplots(size, size, figsize=(10, 10))
 
 # Make the environment
-env = safety_gymnasium.make('SafetyPointGoal0-v0')
+register_sagui_envs()
+# env = safety_gymnasium.make('SafetyPointGoal0-v0')
+env = safety_gymnasium.make('SafetyPointGuide0-v0')
 
 # Plot different dynamics coefficients
-for i, mass in enumerate(np.linspace(0.0001, 0.03, size)):
-    for j, fric in enumerate(np.linspace(0, 0.008, size)):
+for i, mass in enumerate([0.5, 1.0, 1.5]):  # enumerate(np.linspace(0.0001, 0.03, size)):
+    for j, fric in enumerate([0.5, 1.0, 1.5]):  # enumerate(np.linspace(0, 0.008, size)):
         print(f'Mass: {mass}; Fric: {fric}')
 
         # Reset env and modify the dynamics
         env.reset(seed=0)
         terminated, truncated = False, False
         task: BaseTask = env.unwrapped.task
-        modify_dyn(task, {'body_mass': mass, 'dof_frictionloss': fric})
+
+        modify_dyn(task, {'body_mass': mass, 'dof_damping': fric})
 
         # Run the trajectory
         positions = [task.agent.pos]
@@ -51,11 +57,15 @@ for i, mass in enumerate(np.linspace(0.0001, 0.03, size)):
         y_positions = positions[:, 1]
 
         # Subplot
-        ax = axs[i, j]
+        ax: Axes = axs[i, j]
+        ax.set_title(f'Mass={"{:.1f}".format(mass)}; Damp={"{:.1f}".format(fric)}', fontsize=17)
+        ax.set_xlim(0.25, 1.75)
+        ax.set_ylim(-0.5, 1.0)
+        ax.tick_params(labelsize=16)
+        ax.tick_params(labelsize=16)
+
         ax.plot(x_positions, y_positions)
-        ax.set_title(f'Mass={"{:.4f}".format(mass)}; Fric={"{:.4f}".format(fric)}')
-        ax.set_xlim(-0.1, 0.9)
-        ax.set_ylim(-0.1, 0.9)
+
 
 plt.tight_layout()
 plt.savefig('./plot.png')
