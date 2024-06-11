e52b5d589c89bbc8a4d66d0d9b2040ff3f4840bf
mzubia <markel.xubi@gmail.com>
2024-06-11 21:58:15 +0200

    Rename old sagui

--- omnisafe/algorithms/sagui/__init__.py
+++ omnisafe/algorithms/sagui/__init__.py
@@ -4,7 +4,7 @@ from omnisafe.algorithms.sagui.sac_lagb import SACLagB
 from omnisafe.algorithms.sagui.rand_act import SACLagRandAct
 from omnisafe.algorithms.sagui.rand_obs import SACLagRandObs
 from omnisafe.algorithms.sagui.sagui_cs import SaGuiCS
-from omnisafe.algorithms.sagui.old_sagui_cs import OldSaGuiCS
+from omnisafe.algorithms.sagui.sagui_cs_old import OldSaGuiCS
 from omnisafe.algorithms.sagui.ddpg_lag_unfold import DDPGLagUnfold
 from omnisafe.algorithms.sagui.ddpg_lag_randact import DDPGRandAct
 from omnisafe.algorithms.sagui.ddpg_lag_randobs import DDPGRandObs
