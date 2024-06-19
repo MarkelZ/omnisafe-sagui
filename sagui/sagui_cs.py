9526165abe984daa9c09dd6076c5cc7b9d6a8323
mzubia <markel.xubi@gmail.com>
2024-06-19 22:39:53 +0200

    Use the non-robust guide

--- sagui/sagui_cs.py
+++ sagui/sagui_cs.py
@@ -20,7 +20,7 @@ def work(exp_chunk):
             'total_steps': 2000000  # Double the normal amount
         },
         'logger_cfgs': {
-            'save_model_freq': 25
+            'save_model_freq': 100  # 25
         },
     }
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
     # Experiments
     envs = ['SafetyPointStudent1-v0']
     # ['./save_unfold/', './save_randact/', './save_randact_bignoise/', './save_probact/']
-    guides = ['./save_actnoise_0,29_guide1/']
+    guides = ['./save_actnoise_0,0_guide1']  # ['./save_actnoise_0,29_guide1/']
     experiments = [(env_id, guide) for env_id in envs for guide in guides]
 
     coef_dict = {'body_mass': 0.5, 'dof_damping': 0.5}
